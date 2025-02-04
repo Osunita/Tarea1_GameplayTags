7d1ed5c481bb6dd2d61020260b01db9fec94ff5a
Osunita <raulosunaabadias71@gmail.com>
2025-02-04 11:09:33 +0100

    Creación del proyecto

---
+++ Source/Tarea1_GameplayTags/Tarea1_GameplayTags.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Tarea1_GameplayTags : ModuleRules
+{
+	public Tarea1_GameplayTags(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
